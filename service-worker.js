f88885945dddfced452472089eddb594692ccf98
VIKAS <vikas8529611759@gmail.com>
2026-08-10 00:48:38 +0530

    Update CACHE_NAME to version v2

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,9 @@
    wo hamesha network se hi jaati hain (sync data hamesha fresh hona chahiye).
 */
 
-const CACHE_NAME = 'gomati-mart-v1';
+- const CACHE_NAME = 'gomati-mart-v1';
++ const CACHE_NAME = 'gomati-mart-v2';
+
 const APP_SHELL = [
   './',
   './index.html',
